e825d665ce6e814175798d18eaecb2396629de12
joshaeeee <53632884+joshaeeee@users.noreply.github.com>
2026-06-06 14:10:59 +0530

    Prevent forwarded IP rate-limit bypass (#94)

--- mitr-backend/deploy/configure-nginx.sh
+++ mitr-backend/deploy/configure-nginx.sh
@@ -112,7 +112,7 @@ http {
 
       proxy_set_header Host \$host;
       proxy_set_header X-Real-IP \$remote_addr;
-      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-For \$remote_addr;
       proxy_set_header X-Forwarded-Proto \$scheme;
     }
 
@@ -153,7 +153,7 @@ http {
 
       proxy_set_header Host \$host;
       proxy_set_header X-Real-IP \$remote_addr;
-      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-For \$remote_addr;
       proxy_set_header X-Forwarded-Proto \$scheme;
       proxy_set_header Upgrade \$http_upgrade;
       proxy_set_header Connection \$connection_upgrade;
@@ -166,7 +166,7 @@ http {
 
       proxy_set_header Host \$host;
       proxy_set_header X-Real-IP \$remote_addr;
-      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-For \$remote_addr;
       proxy_set_header X-Forwarded-Proto \$scheme;
     }
 
@@ -182,7 +182,7 @@ http {
 
       proxy_set_header Host \$host;
       proxy_set_header X-Real-IP \$remote_addr;
-      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-For \$remote_addr;
       proxy_set_header X-Forwarded-Proto \$scheme;
       proxy_set_header Upgrade \$http_upgrade;
       proxy_set_header Connection \$connection_upgrade;
